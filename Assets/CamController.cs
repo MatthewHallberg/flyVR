c3a3db46facceea1514eca5516fc6b78243082c1
matthew hallberg <matthewhallberg@gmail.com>
2018-06-10 18:35:58 -0400

    turning smooth as butter.

--- Assets/CamController.cs
+++ Assets/CamController.cs
@@ -1,55 +1,100 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-namespace Wrld {
-	public class CamController : MonoBehaviour {
+public class CamController : MonoBehaviour {
 
-		public float speed;
+	public float speed;
 
-		private float currSpeed = 0;
-		private bool shouldMove = false;
+	private float currSpeed = 0;
+	private bool shouldMove = false;
+	private Vector3 tempAngle = Vector3.zero;
+	private float yAngle = 0;
+	private float turnAngleY = 0;
+	private float lastYAngle = 0;
+	private float xAngle = 0;
+	private float zero = 0;
+	private List<float> zeroList = new List<float>();
+	private float tareNum = 0;
 
-		IEnumerator Start () {
-			Application.targetFrameRate = 60;
-			Api.Instance.CameraApi.ClearControlledCamera ();
-			yield return new WaitForSeconds (2f);
-			//wait for map to load
-			shouldMove = true;
-		}
+	IEnumerator Start () {
+		Application.targetFrameRate = 60;
+		//cleared controlled camera just in case 
+		Wrld.Api.Instance.CameraApi.ClearControlledCamera ();
+		//wait for map to load
+		yield return new WaitForSeconds (2f);
+		shouldMove = true;
+	}
 
-		// Update is called once per frame
-		void Update () {
-			if (shouldMove) {
-				//make speed change with x angle
-				currSpeed = speed + ClampAngle (transform.eulerAngles.x);
-				transform.Translate (Vector3.forward * Time.deltaTime * currSpeed);
+	// Update is called once per frame
+	void Update () {
+		if (shouldMove && !zero.Equals(0)) {
+			//make speed change with x angle
+			currSpeed = speed + ClampAngle (transform.eulerAngles.x);
+			transform.Translate (Vector3.forward * Time.deltaTime * currSpeed);
+
+			//set up/down angle
+			tempAngle.x = Mathf.LerpAngle (tempAngle.x, xAngle, Time.deltaTime * 1f);
+
+			//set turn angle
+			if (yAngle < zero - 3 && yAngle < lastYAngle) {
+				turnAngleY -= 5;
+			} else if (yAngle > zero + 3 && yAngle > lastYAngle) {
+				turnAngleY += 5;
 			}
 
-#if UNITY_EDITOR
+			tempAngle.y = Mathf.LerpAngle (tempAngle.y, turnAngleY, Time.deltaTime * 1f);
 
-		if (Input.GetKey (KeyCode.UpArrow)){
-			transform.eulerAngles += new Vector3 (-1, 0, 0);
-		} else if (Input.GetKey (KeyCode.DownArrow)){
-			transform.eulerAngles += new Vector3 (1, 0, 0);
-		} else if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.eulerAngles += new Vector3 (0, -1, 0);
-		} else if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.eulerAngles += new Vector3 (0, 1, 0);
+			transform.eulerAngles = tempAngle;
+			lastYAngle = yAngle;
 		}
 
-#endif
-		}
+//#if UNITY_EDITOR
+//	if (Input.GetKey (KeyCode.UpArrow)){
+//		transform.eulerAngles += new Vector3 (-1, 0, 0);
+//	} else if (Input.GetKey (KeyCode.DownArrow)){
+//		transform.eulerAngles += new Vector3 (1, 0, 0);
+//	} else if (Input.GetKey (KeyCode.LeftArrow)) {
+//		transform.eulerAngles += new Vector3 (0, -1, 0);
+//	} else if (Input.GetKey (KeyCode.RightArrow)) {
+//		transform.eulerAngles += new Vector3 (0, 1, 0);
+//	}
+//#endif
 
-		float ClampAngle (float angle) {
-			if (angle > 180) {
-				angle = Mathf.Clamp (((360 - angle) * -1), -50, 0);
-				return angle;
-			} else if (angle < 0) {
-				return 0;
-			} else {
-				return angle * 2;
-			}
+	}
+
+	public void SetTurnAngle (string value) {
+		if (tareNum < 20) {
+			tareNum++;
+			zeroList.Add (float.Parse (value));
+		} else if (zero.Equals(0)) {
+			zero = zeroList.Average ();
+		} else {
+			yAngle = float.Parse (value);
+		}
+	}
+
+	public void SetUpAngle (string message) {
+		if (message.Contains ("GREEN_DOWN")) {
+			xAngle = 30f;
+		} else if (message.Contains ("GREEN_UP")) {
+			xAngle = 0;
+		} else if (message.Contains ("RED_UP")) {
+			xAngle = 0;
+		} else if (message.Contains ("RED_DOWN")) {
+			xAngle = -30;
+		}
+	}
+
+	float ClampAngle (float angle) {
+		if (angle > 180) {
+			angle = Mathf.Clamp (((360 - angle) * -1), -50, 0);
+			return angle;
+		} else if (angle < 0) {
+			return 0;
+		} else {
+			return angle * 2;
 		}
 	}
 }
---
+++ Assets/MessageController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections;
+
+public class MessageController : MonoBehaviour {
+
+	public CamController camController;
+
+	IEnumerator Start () {
+		yield return new WaitForSeconds (3f);
+		// Create UDP client
+		int receiverPort = 1999;
+		UdpClient receiver = new UdpClient (receiverPort);
+		receiver.BeginReceive (DataReceived, receiver);
+	}
+
+	// This is called whenever data is received
+	private void DataReceived (IAsyncResult ar) {
+		
+		UdpClient c = (UdpClient)ar.AsyncState;
+		IPEndPoint receivedIpEndPoint = new IPEndPoint (IPAddress.Any, 0);
+		Byte [] receivedBytes = c.EndReceive (ar, ref receivedIpEndPoint);
+
+		string packet = System.Text.Encoding.UTF8.GetString (receivedBytes, 0, 20);
+		if (packet.Contains ("_")) {
+			HandleButton (packet);
+		} else {
+			HandlePosition (packet);
+		}
+
+		// Restart listening for udp data packages
+		c.BeginReceive (DataReceived, ar.AsyncState);
+	}
+
+
+	void HandlePosition (string angle) {
+		string [] position = angle.Split (',');
+		camController.SetTurnAngle (position [2]);
+	}
+
+	void HandleButton (string status) {
+		camController.SetUpAngle (status);
+	}
+}
